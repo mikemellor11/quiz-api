258d84275e738127ff28fda93b508afc71d9f8f5
Mike Mellor <mike.mellor@f-grp.com>
2020-05-24 19:43:28 +0100

    prototyping

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ app.use(cors());
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.get('/scores', (req, res) => {
-    res.json([
-        {
-            name: 'Mike',
-            score: 10
-        }
-    ]);
+var tokens = {};
+var games = {};
+
+app.get('/scores/:room', (req, res) => {
+    var game = games[`/${req.params.room}`];
+    res.json(game && game.users.map(d => {
+        return {
+            name: d,
+            score: 0
+        };
+    }) || []);
 });
 
 app.get('/question', (req, res) => {
@@ -26,36 +30,41 @@ app.get('/question', (req, res) => {
     ]);
 });
 
-var tokens = {};
-var games = {};
-
 io.of(/./g).on('connection', function(socket){
-    //console.log(socket.nsp.name, tokens);
-
-    if(!tokens[socket.nsp.name]){
+    if(!games[socket.nsp.name]){
         games[socket.nsp.name] = {
             users: [],
             state: 0
         };
+    }
 
+    if(!tokens[socket.nsp.name]){
         axios.get(`https://opentdb.com/api_token.php?command=request`)
             .then(res => {
                 tokens[socket.nsp.name] = res.data.token;
                 // console.log("ready");
             });
     }
+
     socket.on('username', username => {
         socket.username = username;
         games[socket.nsp.name].users.push(username);
-        console.log(games[socket.nsp.name]);
         socket.nsp.emit('output', '🔵 <i>' + socket.username + ' join the chat..</i>');
+        socket.nsp.emit('update users');
     });
 
     socket.on('disconnect', reason => {
-        console.log(`${socket.username} disconnected`)
-        games[socket.nsp.name].users.splice(games[socket.nsp.name].users.indexOf(socket.username), 1);
-        console.log(games[socket.nsp.name]);
+        var index = games[socket.nsp.name].users.indexOf(socket.username);
+        if(index > -1){
+            games[socket.nsp.name].users. splice(index, 1);
+        }
+
         socket.nsp.emit('output', '🔴 <i>' + socket.username + ' left the chat..</i>');
+        socket.nsp.emit('update users');
+
+        if(!games[socket.nsp.name].users.length){
+            games[socket.nsp.name] = null; 
+        }
     });
 
     socket.on('input', message => socket.nsp.emit('output', '<strong>' + socket.username + '</strong>: ' + message));
