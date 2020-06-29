b66614b8e3f8ecd49346c3b8474badec597d5247
Mike Mellor <mike.mellor@f-grp.com>
2020-06-29 20:33:24 +0100

    split answer functino out into smaller parts

--- services/quiz.js
+++ services/quiz.js
@@ -2,6 +2,11 @@ const axios = require('axios');
 
 var { STATE, games } = require('../globals.js');
 
+var users = {}; ({ 
+    active: users.active,
+    findActive: users.findActive
+} = require('./users.js'));
+
 module.exports = exports = {
     init: (name) => {
         if(games[name]) {
@@ -41,5 +46,41 @@ module.exports = exports = {
         };
 
         game.question.correct = game.question.answers.indexOf(question.correct_answer);
+    },
+    answer: (game, id, index) => {
+        var user = users.findActive(game, id);
+
+        // Only accept a single answer from the same user
+        if(game.question.submitted.findIndex(d => d.id === user.id) === -1){
+            game.question.submitted.push({
+                id: user.id,
+                name: user.name,
+                index: index
+            });
+
+            return true;
+        }
+        
+        return false;
+    },
+    allAnswered: (game) => {
+        if(game.question.submitted.length === users.active(game).length){
+            exports.addScores(game);
+
+            game.question.answer = game.question.correct;
+
+            return true;
+        }
+
+        return false;
+    },
+    addScores: (game) => {
+        game.question.submitted.forEach(d => {
+            var user = users.findActive(game, d.id);
+
+            if(d.index === game.question.correct){
+                user.score += 100;
+            }
+        });
     }
 };
--- sockets/events/answer.js
+++ sockets/events/answer.js
@@ -3,7 +3,9 @@ var { nextQuestion } = require('../../services/quiz.js');
 
 var quiz = {}; ({ 
     getQuestion: quiz.getQuestion,
-    setQuestion: quiz.setQuestion
+    setQuestion: quiz.setQuestion,
+    answer: quiz.answer,
+    allAnswered: quiz.allAnswered
 } = require('../../services/quiz.js'));
 
 var users = {}; ({ 
@@ -15,34 +17,13 @@ module.exports = (socket) => {
     var game = games[socket.nsp.name];
     
     socket.on('answer', ({session, index}) => {
-        var user = users.findActive(game, session.id);
-
-        // Only accept a single answer from the same user
-        if(game.question.submitted.findIndex(d => d.id === user.id) === -1){
-            console.log(`${socket.nsp.name}: ${socket.id} ${user.name} answered ${index}`);
-
-            game.question.submitted.push({
-                id: user.id,
-                name: user.name,
-                index: index
-            });
+        if(quiz.answer(game, session.id, index)){
+            console.log(`${socket.nsp.name}: ${socket.id} ${socket.name} answered ${index}`);
 
             socket.nsp.emit('question');
 
-            if(game.question.submitted.length === users.active(game).length){
-                game.question.submitted.forEach(d => {
-                    // var user = game.users.find(dd => dd.session.id === d.id);
-                    var user = users.findActive(game, d.id);
-
-                    if(d.index === game.question.correct){
-                        user.score += 100;
-                        console.log(`${socket.nsp.name}: ${user.id} ${user.name} answered correctly`);
-                    } else {
-                        console.log(`${socket.nsp.name}: ${user.id} ${user.name} answered incorrectly`);
-                    }
-                });
-
-                game.question.answer = game.question.correct;
+            if(quiz.allAnswered(game)){
+                console.log(`${socket.nsp.name}: All answered`);
 
                 socket.nsp.emit('update users');
 
